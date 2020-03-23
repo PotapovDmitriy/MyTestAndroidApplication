9ed4ec53cecc8c64fd5b1bb3568969f3eef0b160
PotapovDmitriy <lhfujy7477@gmail.com>
2020-03-23 15:50:14 +0500

    Добавлен диалог с пользователем и обработка пунктов меню

--- app/src/main/java/com/example/mytestandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/mytestandroidapplication/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +39,23 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.line1:
+                Toast.makeText(this, "Не надо так!!!!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.line3:
+                Toast.makeText(this, "Сюда можно)))", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Dialog dialog = new Dialog();
+                dialog.show(getSupportFragmentManager(),"Notice");
+        }
+        return  super.onOptionsItemSelected(item);
     }
 
 }
