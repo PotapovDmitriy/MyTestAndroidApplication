f198c9652c32e6b6caa4ea10de6863c2a9ef29e7
Your Name <you@example.com>
2020-05-19 17:02:56 +0500

    Не исправил ошибку, но небольшие изменения внес

--- app/src/main/java/com/example/mytestandroidapplication/Widget.java
+++ app/src/main/java/com/example/mytestandroidapplication/Widget.java
@@ -9,26 +9,22 @@ import android.widget.RemoteViews;
 
 import org.json.JSONObject;
 
+
 public class Widget extends AppWidgetProvider {
     private String city = "Korkino";
-    private String apiKey;
     private AppWidgetManager appMan;
     private RemoteViews remoteViews;
     private ComponentName componentName;
-    private int REQUEST_CODE_PERMISSION_INTERNET = 0;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-
-        System.out.println("hello i am widget");
         appMan = appWidgetManager;
         remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
         componentName = new ComponentName(context, Widget.class);
-        apiKey = "18d82c5a0c9f97611a9864ef7b3c2d34";
-        remoteViews.setTextViewText(R.id.cityField, "Сначала запросите погоду в приложении");
-        remoteViews.setTextViewText(R.id.tempField, "");
-        remoteViews.setTextViewText(R.id.windField, "");
+        String apiKey = "18d82c5a0c9f97611a9864ef7b3c2d34";
+        AsyncWidgetRequest task = new AsyncWidgetRequest();
+        task.execute(city, apiKey);
     }
 
 
