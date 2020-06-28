5392c5e3bea6e1b463bcc624e74f05f6cb78d6e1
Your Name <you@example.com>
2020-06-28 16:44:02 +0500

    Добавление тестирования

--- app/src/androidTest/java/com/example/mytestandroidapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mytestandroidapplication/ExampleInstrumentedTest.java
@@ -2,12 +2,25 @@ package com.example.mytestandroidapplication;
 
 import android.content.Context;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static org.hamcrest.core.StringContains.containsString;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +30,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<NavMainActivity> activityActivityTestRule = new ActivityTestRule<>(NavMainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,4 +41,27 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.mytestandroidapplication", appContext.getPackageName());
     }
+
+    @Test
+    public void checkEditCity() {
+        onView(withId(R.id.etCity)).perform(typeText("Korkino"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.show_weather)).perform(click());
+        onView(withId(R.id.temp)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkListOfWeather() {
+        onView(withId(R.id.etCity)).perform(typeText("Korkino"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.show_weather)).perform(click());
+
+        onView(Matchers.anyOf(
+                withContentDescription("Открыть панель навигации"),
+                withContentDescription("Open navigation drawer")))
+                .perform(click());
+        onView(withText("Список запросов")).perform(click());
+        onView(withId(R.id.content)).check(matches(withText(containsString("Korkino"))));
+    }
+
 }
